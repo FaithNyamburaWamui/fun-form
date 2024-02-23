9c02cb7ed10f15e378b57d3500f019be74741d81
Faith-Nyambura <faithnyamburawamui@gmail.com>
2024-02-23 13:27:22 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+fun main() {
+    println("hello faith")
+    modulus(24, 33)
+    sumOfNumbers(6, 8, 9, 4)
+    println("I love to read novels and have nature walks")
+
+
+
+
+}
+fun printMyName(name :String){
+    println(name)
+
+}
+
+fun modulus(num1: Int, num2: Int): Int{
+    var  result = num1 % num2
+    return  result
+
+}
+
+fun  sumOfNumbers(num1: Int, num2: Int, num3: Int, num4: Int ): Int{
+    var result=num1 + num2 +num3 +num4
+    return  result
+
+}
+
+fun printIntrest (name: String){
+    println(name)
+}
+
+
+
